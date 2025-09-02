53313cb0191125562cf4ddc0decd6b238022d005
boradogan <doan.bora@gmail.com>
2025-09-02 22:11:39 +0300

    added basic image caroussel logic

--- src/components/ImageCaroussel.jsx
+++ src/components/ImageCaroussel.jsx
@@ -1,16 +1,35 @@
+import { useEffect, useRef, useState } from "react"
 export function ImageCaroussel(){
+
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const goToNext = () => {
+        setCurrentIndex(prevIndex => 
+            prevIndex === 6 - 1 ? 0 : prevIndex + 1
+        );
+    };
+
+    const goToPrevious = () => {
+        setCurrentIndex(prevIndex => 
+            prevIndex === 0 ? 6 - 1 : prevIndex - 1
+        );
+    };
+    const containerStyle = {
+        transform: `translateX(-${currentIndex * 25}%)`
+    };
+   
     return(
         <div className="image-caroussel">
+            
             <div className="button-wrapper">
                 <ul>
                     <li>
-                        <button></button>
+                        <button onClick={goToPrevious}></button>
                     </li>
                     <li>
                         <button></button>
                     </li>
                     <li>
-                        <button></button>
+                        <button onClick={goToNext}></button>
                     </li>
                 </ul>
 
@@ -19,6 +38,28 @@ export function ImageCaroussel(){
 
             </div>
             <div className="left direct">
+
+            </div>
+            <div className="caroussel-image-container"  style={containerStyle}>
+                <div className="caroussel-item-big">
+                    <img src="https://placehold.co/100" alt="" />
+                    
+                </div>
+                <div className="caroussel-item-big">
+                    <img src="https://placehold.co/200" alt="" />
+                </div>
+                <div className="caroussel-item-big">
+                    <img src="https://placehold.co/300" alt="" />
+                </div>
+                <div className="caroussel-item-big">
+                    <img src="https://placehold.co/400" alt="" />
+                </div>
+                <div className="caroussel-item-big">
+                    <img src="https://placehold.co/500" alt="" />
+                </div>
+                <div className="caroussel-item-big">
+                    <img src="https://placehold.co/600" alt="" />
+                </div>
 
             </div>
             
